686b07b31c563daefcce652d29cb122df0376d51
Liliya <liliyavoloshine@gmail.com>
2021-03-23 11:39:11 +0400

    fix: refactor hard coded connection string to Heroku config var

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const uri = process.env.MONGODB_URI;
 global.Vocab = require('./api/models/vocabModel')
 const routes = require('./api/routes/vocabRoutes')
 mongoose.Promise = global.Promise
 mongoose.set('useFindAndModify', false)
 mongoose.connect(
-  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@unpopular-vocabular-clu.wiqoo.mongodb.net/unpopular-vocabular`,
+  uri,
+  // `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@unpopular-vocabular-clu.wiqoo.mongodb.net/unpopular-vocabular`,
   { useNewUrlParser: true, useUnifiedTopology: true }
 )
 

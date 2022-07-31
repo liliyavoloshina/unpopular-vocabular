ec6e063ed97f466dbc0b8dcc0089d1cd9eb52acc
Liliya <liliyavoloshine@gmail.com>
2022-07-31 14:33:16 +0600

    fix: static files

--- server/app.js
+++ server/app.js
@@ -13,6 +13,14 @@ dotenv.config({ path: './.env' })
 
 const app = express()
 
+//here we are configuring dist to serve app files
+app.use('/', express.static(path.join('/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join('/dist/index.html'))
+})
+
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({ credentials: true, origin: [process.env.DEV_CLIENT_URL, process.env.PROD_CLIENT_URL] }))
@@ -21,6 +29,6 @@ app.use('/api/v1/user', userRouter)
 app.use('/api/v1', authRouter)
 app.use(errorSender)
 
-app.use(express.static(path.join('/dist')))
+// app.use(express.static(path.join('/dist')))
 
 export default app

1724d620b1edfd8410ff2e2c568562d75cb0037f
Liliya <liliyavoloshine@gmail.com>
2022-07-31 14:22:25 +0600

    fix: path

--- server/server.js
+++ server/server.js
@@ -9,9 +9,7 @@ dotenv.config({ path: './.env' })
 const db = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.oedfl.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`
 mongoose.connect(db, { useNewUrlParser: true })
 
-const PORT = process.env.DEV_SERVER_PORT
-
-const server = app.listen(PORT, () => console.log(`Listening to: http://127.0.0.1:${PORT}`))
+const server = app.listen(process.env.PORT || 3000, () => console.log(`Listening to: http://127.0.0.1:${process.env.PORT || 3000}`))
 
 process.on('unhandledRejection', (err) => {
   console.log(err)
